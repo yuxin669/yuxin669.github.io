2aecbb44d3df93e7a9896570b9b8fcfd1e86ae2a
yuxin669 <2115585692@qq.com>
2025-02-15 23:54:58 +0000

    Site updated: 2025-02-15 23:54:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/1.html","2025/02/15/hello-world/"];function toRandomPost(){
+var posts=["/1.html","/2.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
