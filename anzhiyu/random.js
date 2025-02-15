c86955bb16db4eac3e88e3eb859aa16f7b276948
yuxin669 <2115585692@qq.com>
2025-02-15 21:56:25 +0000

    Site updated: 2025-02-15 21:56:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/15/hello-world/"];function toRandomPost(){
+var posts=["/1.html","2025/02/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
